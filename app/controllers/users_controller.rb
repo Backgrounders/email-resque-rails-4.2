7f470efc35c00220bb039bfde31e7bed1f9e9def
Sunny Mittal <sunnymittal2003@gmail.com>
2014-10-15 11:33:38 -0600

    Fix Rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
@@ -21,7 +20,8 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :content)
-    end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :content)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015024817) do
+ActiveRecord::Schema.define(version: 20_141_015_024_817) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text     "content"
+  create_table 'users', force: true do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.text 'content'
   end
 
 end
